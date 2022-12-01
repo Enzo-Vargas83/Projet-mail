82171ac0ad1dbc115fb93636ca31e8e9a4e42e3f
Enzo-vargas83 <enzo.vargas@etu.univ-amu.fr>
2022-12-01 11:56:31 +0100

    add functions GenererVhs and displayActeur to gestion.c

--- gestion.c
+++ gestion.c
@@ -16,15 +16,39 @@ typedef struct
    char ActeurStar[SIZE_CHAINE];
    char ActeurStar2[SIZE_CHAINE];
    char ActeurStar3[SIZE_CHAINE];
-} VHS
+} VHS;
 typedef struct
 {
    char NumTel[SIZE_CHAINE];
    char Nom[SIZE_CHAINE];
    char Mail[SIZE_CHAINE];
-   int NbLocation = 0;
-   int PtsFidelite = 0; 
-} CLIENT
+   int NbLocation;
+   int PtsFidelite;
+} CLIENT;
+
+void GenererVhs(VHS *vhs);
+void displayActeur(VHS *vhs);
+
+void GenererVhs(VHS *vhs)
+{
+   char str[SIZE_CHAINE][SIZE_CHAINE] = {"Johnny Cos", "Manuel Ferrari"};
+   for(int i = 0; i < 2; i++)
+   {
+   	for(int y = 0; y < SIZE_CHAINE; y++)
+   	{
+   		vhs->Nom[y] = str[i][y];
+   	}
+   	printf("\n");
+   } 
+}
+
+void displayActeur(VHS *vhs)
+{
+	printf("nom : %s\n", vhs->Nom);
+}
 
 int main( int argc, char * argv[] ) {
+	VHS vhs;
+	GenererVhs(&vhs);
+	displayActeur(&vhs);
 }
