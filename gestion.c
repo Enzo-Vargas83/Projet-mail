6e81666f966f3939de17d103f3ea9383db084174
Enzo-vargas83 <enzo.vargas@etu.univ-amu.fr>
2022-12-01 11:29:03 +0100

    add struct VHS to gestion.c

--- gestion.c
+++ gestion.c
@@ -5,15 +5,25 @@
 #include <time.h>
 #include <string.h>
 
-#define SIZE_CHAINE 10
+#define SIZE_CHAINE 15
 
 typedef struct
 {
-   char numTel[SIZE_CHAINE];
-   char nom[SIZE_CHAINE];
-   char mail[SIZE_CHAINE];
-   int nbLocation = 0;
-   int ptsFidelite = 0; 
+   int Duree;
+   char ID[SIZE_CHAINE];
+   char Nom[SIZE_CHAINE];
+   char Categorie[SIZE_CHAINE];
+   char ActeurStar[SIZE_CHAINE];
+   char ActeurStar2[SIZE_CHAINE];
+   char ActeurStar3[SIZE_CHAINE];
+} VHS
+typedef struct
+{
+   char NumTel[SIZE_CHAINE];
+   char Nom[SIZE_CHAINE];
+   char Mail[SIZE_CHAINE];
+   int NbLocation = 0;
+   int PtsFidelite = 0; 
 } CLIENT
 
 int main( int argc, char * argv[] ) {
