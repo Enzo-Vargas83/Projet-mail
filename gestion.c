4ec6e578f533ee85bfa6e1263b0c8a3833c21d52
Enzo-vargas83 <enzo.vargas@etu.univ-amu.fr>
2022-12-01 11:11:23 +0100

    add struct client to gestion.c

--- gestion.c
+++ gestion.c
@@ -5,6 +5,16 @@
 #include <time.h>
 #include <string.h>
 
+#define SIZE_CHAINE 10
+
+typedef struct
+{
+   char numTel[SIZE_CHAINE];
+   char nom[SIZE_CHAINE];
+   char mail[SIZE_CHAINE];
+   int nbLocation = 0;
+   int ptsFidelite = 0; 
+} CLIENT
 
 int main( int argc, char * argv[] ) {
 }
